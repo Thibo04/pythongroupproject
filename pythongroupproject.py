51765652b6a4d9b42cdf8140b3af9b4e763945a3
Thibo04 <thibo.fries@gmx.ch>
2025-11-27 22:09:40 +0100

    Add files via upload

---
+++ pythongroupproject.py
@@ -0,0 +1,33 @@
+#Python group project, Cybersecurity
+
+#Ihr könnt euren code gleich unter dem jeweiligen Kommentar einfügen. Für das separate erstellen/bearbeiten würde ich zuerst
+#in einem offline-fenster arbeiten
+
+# 1. option: network scanner
+
+
+
+
+
+
+# 2. option: Service & OS-Fingerprinting
+
+
+
+
+
+
+# 3. option: Firewall-/DoS-Simulation
+
+
+
+
+
+# 4. option: Reporting & Logging
+
+
+
+
+
+# eventually a 5. option: Stealth Mode (only if needed and enough time)
+
