c78896f6828c5b91d70ac676df9bd572d1d6bfa0
Thibo04 <thibo.fries@gmx.ch>
2025-12-30 14:05:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -187,7 +187,7 @@ def main() -> None:
                     try:
                         choice = int(choice)
                         if choice == 1:
-                            set_stealth_mode()
+                            set_stealth_mode(True)
                             print("The results of this execution are now available in the logs-folder")
                             break
                         elif choice == 2:
